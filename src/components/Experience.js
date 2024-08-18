1f1a328a6e2deee2bfdf6badf5d16a579645cd25
Henry Wang <henrywang3@g.ucla.edu>
2024-08-18 14:34:10 -0700

    updating website

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,8 +7,11 @@ const Experience = () => {
     return (
         <section id="experience">
             <h2>Experience</h2>
+            <b>Current:</b> 
+            <li>Software Engineer @ Meta (Reality Labs)</li>
+            <b>Formerly:</b>
             <li>Augmented Reality (AR) Engineer Intern @ Snap Inc</li>
-            <li>Software Engineer Intern @ Meta (Facebook Reality Labs)</li>
+            <li>Software Engineer Intern @ Meta (Reality Labs)</li>
             <li>Software Engineer Intern @ Engine Gaming (NASDAQ:GAME)</li>
             <li>Software Engineer Intern @ Daily Bruin (internal tools)</li>
             <li>Co-President @ ACM Studio</li>
